6968789bd50f0fde9d6cd5d1763a99ee8e6f7d78
Satyam Saw <85004499+stymsaw@users.noreply.github.com>
2022-04-17 17:50:44 +0530

    Update Array of 0's, 1's & 2's  Sort without Algo.java

--- Array of 0's, 1's & 2's  Sort without Algo.java
+++ Array of 0's, 1's & 2's  Sort without Algo.java
@@ -39,3 +39,11 @@ public class Main {
 
     }
 }
+
+/* 
+1.  create three variables to keep the count of 0's, 1's & 2's in the given array.
+2. run three loops for refilling the array in sorted manner without using sorting algo.
+    a. loop1 : i = 0 to i < 0's count
+    b. loop2 : i = 0's count to i < (0's + 1's) count.
+    c. loop 3 : from i to last index of array.
+*/
