5bb694d40aacd54b395b3ef58b518608cd19ecc4
Satyam Saw <satyamatibarts@gmail.com>
2024-01-10 08:50:32 +0530

    MergeSort & SelectionSort Impl
    Date: 10th Jan 2024

---
+++ sorting_algo/impl/InsertionSort.java
@@ -0,0 +1,12 @@
+package com.company.sorting_algo.impl;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 5, 2, 3, 11, 7, 5, 9};
+
+
+    }
+
+    
+}
